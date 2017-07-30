b6627fa95426366892897921b7804c5066aace8e
Marat Shaymardanov <yeffasol@mail.ru>
2017-07-30 17:03:48 +0300

    Fix js-modules and some bugs. It takes 1 hours

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,13 +1,13 @@
 export default function menu() {
   var menuIcon = document.querySelector(".header__menu-icon"),
     menu = document.querySelector(".header__menu"),
-    headerForm= document.querySelector(".header__form"),
-    success= document.querySelector(".header__success"),
-    successClose= document.querySelector(".header__success-close"),
+    headerForm = document.querySelector(".header__form"),
+    success = document.querySelector(".header__success"),
+    successClose = document.querySelector(".header__success-close"),
     contactIcon = document.querySelector(".header__contact-icon"),
     headerBtn = document.querySelector(".header__btn"),
-    overlay=document.querySelector(".overlay"),
-    popupClose= document.querySelector(".header__popup-close"),
+    overlay = document.querySelector(".overlay"),
+    popupClose = document.querySelector(".header__popup-close"),
     popup = document.querySelector(".header__popup");
 
   menuIcon.addEventListener("click", function () {
@@ -18,18 +18,20 @@ export default function menu() {
   });
   headerBtn.addEventListener("click", function () {
     popup.classList.toggle("js-hided");
-    overlay.style.height= 100+"%";
+    overlay.style.height = 100 + "%";
   });
- popupClose.addEventListener("click", function () {
+  popupClose.addEventListener("click", function () {
     popup.classList.add("js-hided");
+    overlay.style.height = 0;
   });
- headerForm.addEventListener("submit", function (e) {
-   popup.classList.add("js-hided");
-   success.classList.remove("js-hided");
-   e.preventDefault();
+  headerForm.addEventListener("submit", function (e) {
+    popup.classList.add("js-hided");
+    success.classList.remove("js-hided");
+    overlay.style.height = 100 + "%";
+    e.preventDefault();
   });
- successClose.addEventListener("click", function () {
-   success.classList.add("js-hided");
-   overlay.style.height= 0;
+  successClose.addEventListener("click", function () {
+    success.classList.add("js-hided");
+    overlay.style.height = 0;
   });
 }
